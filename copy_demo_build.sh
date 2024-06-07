e89881a927fffe05ea47eec7355bedc65f96ffe7
Byron Wasti <byron.wasti@gmail.com>
2024-06-07 11:58:30 -0400

    Update demo

---
+++ copy_demo_build.sh
@@ -0,0 +1,13 @@
+rm -r static/_file
+rm -r static/_npm
+rm -r static/_import
+rm -r static/_observablehq
+
+cp -r ../autobalter/demo/dist/* static/
+
+mv static/index.html static/demo-index.html
+
+fastmod --accept-all -d static/ demo-history demo-history.html
+fastmod --accept-all -d static/ demo-api-overview demo-api-overview.html
+fastmod --accept-all -d static/ demo-report demo-report.html
+fastmod --accept-all -d static/ demo-specification demo-specification.html
